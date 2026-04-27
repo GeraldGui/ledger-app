771f19189b6b674bb53353cba42f23f6d0effbaf
Gerald G <geraldguignard2@gmail.com>
2026-04-27 10:44:03 -0400

    Made Variables for the class and made the Constructor, Getter and Setters.

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
 
 public class Transaction {
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(String description, String vendor, double amount) {
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
